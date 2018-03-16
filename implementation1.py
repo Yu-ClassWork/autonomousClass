44b90f89e6ec21287783101bef74364ee2e10a67
Kevin Li Yu <klyu@vt.edu>
2018-03-15 22:32:14 -0400

    added __init__.py to allow for importing/finished reward1

--- implementation1.py
+++ implementation1.py
@@ -4,7 +4,40 @@
 # two agents face two doors. one door has a hungry tiger and the other door has money. the position of these are unknown.
 # 
 import math
+import importlib
 from random import *
+from rewards import *
+
+def initializing():
+	tigerDoor = randint(1,2) # ground truth of where the tiger is (1: tiger is behind 1 SL, 2: tiger is behind 2 SR)
+	agent0 = 0.5 # belief that the tiger is behind door 1 for agent0
+	agent1 = 0.5 # belief that the tiger is behind door 1 for agent1
+	print("Door the tiger is behind = " + repr(tigerDoor))
+	return tigerDoor, agent0, agent1
+
+def choosingAction(agent0, agent1):
+	if agent0 == 0.5:
+		action0 = randint(1,3) # random policy for agent0
+	else:
+		if agent0 > 0.5:
+			action0 = 2
+		elif agent0 < 0.5:
+			action0 = 1
+		else:
+			print("Something went wrong! agent0 action0")
+	if agent1 == 0.5:
+		action1 = randint(1,3) # random policy for agent1
+	else:
+		if agent1 > 0.5:
+			action1 = 2
+		elif agent1 < 0.5:
+			action1 = 1
+		else:
+			print("Something went wrong! agent1 action1")
+	print("action0 = " + repr(action0) + " action1 = " + repr(action1))	
+	return action0, action1
+
+
 
 def main():
 	agent0 = []
@@ -14,95 +47,13 @@ def main():
 	state = 0
 	reward = 0
 	totalReward = 0
-	while true():
+	listened = 0
+	while True:
 		if listened == 0:
-			tigerDoor = randint(1,2) # ground truth of where the tiger is (1: tiger is behind 1 SL, 2: tiger is behind 2 SR)
-			agent0 = 0.5 # belief that the tiger is behind door 1 for agent0
-			agent1 = 0.5 # belief that the tiger is behind door 1 for agent1
-		print("state = " + repr(tigerDoor))
+			tigerDoor, agent0, agent1 = initializing()
 
-		if agent0 == 0.5:
-			action0 = randint(1,3) # random policy for agent0
-		else:
-			if agent0 > 0.5:
-				action0 = 2
-			elif agent0 < 0.5:
-				action0 = 1
-			else:
-				print("Something went wrong! agent0 action0")
-		if agent1 == 0.5:
-			action1 = randint(1,3) # random policy for agent1
-		else:
-			if agent1 > 0.5:
-				action1 = 2
-			elif agent1 < 0.5:
-				action1 = 1
-			else:
-				print("Something went wrong! agent1 action1")
-		print("action0 = " + repr(action0) + "action1 = " + repr(action1))
-
-		if tigerDoor == 1:
-			if action0 == 2 and action1 == 2:
-				listened = 0
-				reward = 20
-			elif action0 == 1 and action1 == 1:
-				listened = 0
-				reward = -50
-			elif action0 == 2 and action1 == 1:	
-				listened = 0
-				reward = -100
-			elif action0 == 1 and action1 == 2:
-				listened = 0
-				reward = -100
-			elif action0 == 3 and action1 == 3:
-				agent0 = 0.85
-				agent1 = 0.85
-				listened = 1
-				reward = -2
-			elif action0 == 3 and action1 == 2:
-				listened = 0
-				reward = 9
-			elif action0 == 2 and action1 == 3:
-				listened = 0
-				reward = 9
-			elif action0 == 3 and action1 == 1:
-				listened = 0
-				reward = -101
-			elif action0 == 1 and action1 == 3:
-				listened = 0
-				reward = -101
-		elif tigerDoor == 2:
-			if action0 == 2 and action1 == 2:
-				listened = 0
-				reward = -50
-			elif action0 == 1 and action1 == 1:
-				listened = 0
-				reward = 20
-			elif action0 == 2 and action1 == 1:	
-				listened = 0
-				reward = -100
-			elif action0 == 1 and action1 == 2:
-				listened = 0
-				reward = -100
-			elif action0 == 3 and action1 == 3:
-				agent0 = 0.15
-				agent1 = 0.15
-				listened = 1
-				reward = -2
-			elif action0 == 3 and action1 == 2:
-				listened = 0
-				reward = -101
-			elif action0 == 2 and action1 == 3:
-				listened = 0
-				reward = -101
-			elif action0 == 3 and action1 == 1:
-				listened = 0
-				reward = 9
-			elif action0 == 1 and action1 == 3:
-				listened = 0
-				reward = 9
-		else:
-			print("Something went wrong with creating the tigerDoor")
+		action0, action1 = choosingAction(agent0, agent1)
+		listened, reward = reward1(tigerDoor, action0, action1)
 
 		totalReward = totalReward + reward
 		print("totalReward: " + repr(totalReward))
