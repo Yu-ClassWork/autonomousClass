e75770ebd92485e3c48497b7664ec79432fa1c27
Kevin Li Yu <klyu@vt.edu>
2018-03-27 10:55:49 -0400

    created plots for implementation1

--- implementation1.py
+++ implementation1.py
@@ -2,10 +2,12 @@
 # classic triger problem
 # PROBLEM DESCRIPTION
 # two agents face two doors. one door has a hungry tiger and the other door has money. the position of these are unknown.
-# 
+
 import math
 from random import *
 from rewards import *
+from policy import *
+import matplotlib.pyplot as plt
 
 def initializing():
 	tigerDoor = randint(1,2) # ground truth of where the tiger is (1: tiger is behind 1 SL, 2: tiger is behind 2 SR)
@@ -14,28 +16,6 @@ def initializing():
 	print("Door the tiger is behind = " + repr(tigerDoor))
 	return tigerDoor, agent0, agent1
 
-def choosingAction(agent0, agent1):
-	if agent0 == 0.5:
-		action0 = randint(1,3) # random policy for agent0
-	else:
-		if agent0 > 0.5:
-			action0 = 2
-		elif agent0 < 0.5:
-			action0 = 1
-		else:
-			print("Something went wrong! agent0 action0")
-	if agent1 == 0.5:
-		action1 = randint(1,3) # random policy for agent1
-	else:
-		if agent1 > 0.5:
-			action1 = 2
-		elif agent1 < 0.5:
-			action1 = 1
-		else:
-			print("Something went wrong! agent1 action1")
-	print("action0 = " + repr(action0) + " action1 = " + repr(action1))	
-	return action0, action1
-
 def main():
 	agent0 = []
 	agent1 = []
@@ -43,19 +23,38 @@ def main():
 	policy1 = [0]
 	state = 0
 	reward = 0
-	totalReward = 0
+	totalReward = [0]
 	listened = 0
-	while True:
-		if listened == 0:
-			tigerDoor, agent0, agent1 = initializing()
+	i = 0
+	policyType = input("What policy do you want to use?")
+	policyType = int(policyType)
+	# while True:
+	for i in range(0,300):
+		if 1 <= policyType <= 3:
+			if listened == 0:
+				tigerDoor, agent0, agent1 = initializing()
+			if policyType == 1:
+				action0, action1 = policyA(agent0, agent1)
+			elif policyType == 2:
+				action0, action1 = policyB(agent0, agent1)
+			elif policyType == 3:
+				action0, action1 = policyC(agent0, agent1)
+			
 
-		action0, action1 = choosingAction(agent0, agent1)
-		listened, reward, agent0, agent1 = rewardA(tigerDoor, action0, action1, agent0, agent1)
-		print(agent0, agent1)
+			listened, reward, agent0, agent1 = rewardA(tigerDoor, action0, action1, agent0, agent1)
+			print(agent0, agent1)
+			i = i+1
+			totalReward.append(totalReward[i-1] + reward)
+			# print("totalReward: " + repr(totalReward[i]))
+			# input("Press Enter to for next round!")
+		else:
+			policyType = input("Not a suitable policy. Pick a number 1, 2 or 3.")
+			policyType = int(policyType)
+		# except KeyboardInterrupt:
+		# 	break
+	plt.plot(range(0,i+1), totalReward)
+	plt.show()
 
-		totalReward = totalReward + reward
-		print("totalReward: " + repr(totalReward))
-		input("Press Enter to for next round!")
 
 if __name__ == '__main__':
 	main()
