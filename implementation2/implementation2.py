9a1ac409c2ce4be73bddb8155dd89aba319d4487
Kevin Li Yu <klyu@vt.edu>
2018-04-16 14:57:56 -0400

    increased sensing range/added figures for presentation

--- implementation2/implementation2.py
+++ implementation2/implementation2.py
@@ -9,9 +9,6 @@ global m, n
 m = 13
 n = 18
 
-# def KTC(): # Kick the Can Game
-
-
 def createGrid(value):
 	global m, n
 	# Creates a list containing n lists, each of m items, all set to 0
@@ -19,7 +16,6 @@ def createGrid(value):
 	return matrix
 
 def up(stateX, stateY):
-	# print("up")
 	global m
 	newStateX = stateX
 	newStateY = stateY +1
@@ -28,7 +24,6 @@ def up(stateX, stateY):
 	return newStateX, newStateY
 
 def right(stateX, stateY):
-	# print("right")
 	global n
 	newStateX = stateX +1
 	newStateY = stateY
@@ -37,7 +32,6 @@ def right(stateX, stateY):
 	return newStateX, newStateY
 
 def down(stateX, stateY):
-	# print("down")
 	newStateX = stateX
 	newStateY = stateY -1
 	if newStateY <0:
@@ -45,7 +39,6 @@ def down(stateX, stateY):
 	return newStateX, newStateY
 
 def left(stateX, stateY):
-	# print("left")
 	newStateX = stateX -1
 	newStateY = stateY
 	if newStateX <0:
@@ -53,7 +46,6 @@ def left(stateX, stateY):
 	return newStateX, newStateY
 
 def collect(agent, rewards, prevAction):
-	# print("COLLECT")
 	global m,n
 	lock = 0
 	if (agent[0]+1) < n and lock == 0 and prevAction != 2:
@@ -73,14 +65,10 @@ def collect(agent, rewards, prevAction):
 			action = 3
 			lock = 1
 	if lock == 0:
-		# print("random")
 		action = randint(1,4)
-	# print(action)
-	# print("\n")
 	return action
 
 def collectRowCol(agent, rewards, prevAction):
-	# print("COLLECTROWCOL")
 	global m,n
 	lock = 0
 	col = np.sum(rewards, axis=0)
@@ -92,20 +80,14 @@ def collectRowCol(agent, rewards, prevAction):
 	for i in range(agent[0]+1, n):
 		rowSumDown = rowSumDown + row[i]
 	for i in range(0, agent[0]):
-		# print("1")
 		rowSumUp = rowSumUp + row[i]
 	for i in range(agent[1]+1, m):
 		colSumRight = colSumRight + col[i]
 	for i in range(0, agent[1]):
 		colSumLeft = colSumLeft + col[i]
-	# print("agent position:" + repr(agent[0]) + repr(agent[1]))
-	# print("row:" + repr(row) + " col:" + repr(col))
-	# print("rowSumDown:" + repr(rowSumDown) + " rowSumUp:" + repr(rowSumUp) + " colSumRight:" + repr(colSumRight) + " colSumLeft:" + repr(colSumLeft))
 	sectorRewards = [rowSumDown, rowSumUp, colSumRight, colSumLeft]
 	temp = max(sectorRewards)
 	locationMax = [i for i, j in enumerate(sectorRewards) if j == temp]
-	# print(locationMax)
-	# print(np.argmax(sectorRewards))
 	if locationMax[0] == 0 and prevAction != 2:
 		action = 2
 	elif locationMax[0] == 1 and prevAction != 4:
@@ -116,9 +98,6 @@ def collectRowCol(agent, rewards, prevAction):
 		action = 3
 	else:
 		action = randint(1,4)
-		# print("random")
-	# print(action)
-	# print("\n")
 	return action
 
 
@@ -146,20 +125,20 @@ def agentMovement(agent, locations, rewards, prevAction, type):
 	if type == 0:
 		action = randint(1,4)
 	else:
-		if (agent[0]+1) < n:
-			if locations[agent[0]+1][agent[1]] == 8:
+		if (agent[0]+1) < n and (agent[1]+1) < m:
+			if locations[agent[0]+1][agent[1]] == 8 or locations[agent[0]+1][agent[1]+1]:
 				action = 4
 				lock = 1
-		if (agent[1]+1) < m:
-			if locations[agent[0]][agent[1]+1] == 8:
+		if (agent[1]+1) < m and (agent[0]-1) > -1:
+			if locations[agent[0]][agent[1]+1] == 8 or locations[agent[0]-1][agent[1]+1]:
 				action = 3
 				lock = 1
-		if (agent[0]-1) > -1:
-			if locations[agent[0]-1][agent[1]] == 8:
+		if (agent[0]-1) > -1 and (agent[1]-1) > -1:
+			if locations[agent[0]-1][agent[1]] == 8 or locations[agent[0]-1][agent[1]-1]:
 				action = 2
 				lock = 1
-		if (agent[1]-1) > -1:
-			if locations[agent[0]][agent[1]-1] == 8:
+		if (agent[1]-1) > -1 and (agent[0]+1) < n:
+			if locations[agent[0]][agent[1]-1] == 8 or locations[agent[0]+1][agent[1]-1]:
 				action = 1
 				lock = 1
 		if lock == 0:
@@ -178,8 +157,6 @@ def updateWorld(locationWorld, rewardWorld, numAgents, agents):
 	locationWorld[agents[0][0]][agents[0][1]] = 8
 	for i in range(1,len(agents)):
 		locationWorld[agents[i][0]][agents[i][1]] = 6
-		# locationWorld[agent1[0]][agent1[1]] = 6
-		# locationWorld[agent2[0]][agent2[1]] = 6
 	rewardWorld[agent1[0]][agent1[1]] = 0
 	rewardWorld[agent2[0]][agent2[1]] = 0
 	return locationWorld, rewardWorld
@@ -216,8 +193,8 @@ if __name__ == '__main__':
 			stateY = newStateY
 			# raw_input()
 			agent0, action0 = agentMovement(agent0, locationWorld, world, action, 0)
-			agent1, action1 = agentMovement(agent1, locationWorld, world, action, 0)
-			agent2, action2 = agentMovement(agent2, locationWorld, world, action1, 0)
+			agent1, action1 = agentMovement(agent1, locationWorld, world, action, 1)
+			agent2, action2 = agentMovement(agent2, locationWorld, world, action1, 1)
 			tempAgent0 = world[agent0[0]][agent0[1]]
 			if world[agent1[0]][agent1[1]] == 1:
 				jointReward = jointReward + 1
